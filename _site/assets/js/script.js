08686040ae43363fcf6054c489c3fd3ecf802519
Kaleshe Alleyne-Vassel <kaleshe.alleyne-vassel@krystal.uk>
2022-06-01 17:30:50 +0100

    Removed site folder

--- _site/assets/js/script.js
+++
@@ -1,4 +0,0 @@
-const hamburger = document.querySelector('#hamburger');
-const menu = document.querySelector('#menu');
-
-hamburger.addEventListener('click', () => { menu.classList.toggle('is-open') });
