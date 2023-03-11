55f243e4abbc5e0d76fa7533f669f8c2acc8c116
Kaleshe Alleyne-Vassel <kaleshe.alleyne-vassel@krystal.uk>
2023-03-11 23:35:48 +0000

    tidied overall styling and added noreferrer noopener to links

--- _data/blogposts.js
+++ _data/blogposts.js
@@ -1,84 +1,6 @@
 const fetch = require("node-fetch");
 
-// function to get blogposts
 async function getAllBlogposts() {
-    // max number of records to fetch per query
-    // const recordsPerQuery = 100;
-
-    // // number of records to skip (start at 0)
-    // let recordsToSkip = 0;
-
-    // let makeNewQuery = true;
-
-    // let blogposts = [];
-
-    // // make queries until makeNewQuery is set to false
-    // while (makeNewQuery) {
-    //     try {
-    //         // initiate fetch
-    //         const data = await fetch("http://localhost:1337/articles", {
-    //             method: "POST",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 Accept: "application/json",
-    //             },
-    //             body: JSON.stringify({
-    //                 query: `{
-    //                     articles {
-    //                     id
-    //                     Title
-    //                     Content
-    //                     published_at
-    //                     Cover
-    //                     Slug
-    //                     }
-    //                 }`,
-    //             }),
-    //         });
-
-    //         console.log(data);
-
-    //         // store the JSON response when promise resolves
-    //         const response = await data.json();
-
-    //         // handle CMS errors
-    //         if (response.errors) {
-    //             let errors = response.errors;
-    //             errors.map((error) => {
-    //                 console.log(error.message);
-    //             });
-    //             throw new Error("Houston... We have a CMS problem");
-    //         }
-
-    //         // update blogpost array with the data from the JSON response
-    //         blogposts = blogposts.concat(response.data.articles);
-
-    //         // prepare for next query
-    //         recordsToSkip += recordsPerQuery;
-
-    //         // stop querying if we are getting back less than the records we fetch per query
-    //         if (response.data.articles.length < recordsPerQuery) {
-    //             makeNewQuery = false;
-    //         }
-    //     } catch (error) {
-    //         throw new Error(error);
-    //     }
-    // }
-
-    // // format blogposts objects
-    // const blogpostsFormatted = blogposts.map((item) => {
-    //     return {
-    //         id: item.id,
-    //         title: item.Title,
-    //         slug: item.Slug,
-    //         body: item.Content,
-    //         cover: item.Cover,
-    //         date: item.published_at
-    //     };
-    // });
-
-    // // return formatted blogposts
-    // return blogpostsFormatted;
 
     function parseMarkdown(markdownText) {
         const htmlText = markdownText
@@ -119,4 +41,4 @@ async function getAllBlogposts() {
 
 
 // export for 11ty
-module.exports = getAllBlogposts
+// module.exports = getAllBlogposts
--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'bee9879ca06a4af9c992baccb761672a'),
-    },
-  },
-});
